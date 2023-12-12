a4440b68404b7ea981399f54a5aa0f756d3dd972
Tlafay1 <tlclash12@gmail.com>
2023-12-12 14:45:54 +0100

    clean

--- level2/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <errno.h>
-int main(int ac, char **av) 
-{
-    printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", EACCES, EACCES, EACCES, EACCES, EAGAIN, EFAULT, EINVAL, EIO, EISDIR, ELIBBAD, ELOOP, ELOOP, EMFILE, ENAMETOOLONG, ENFILE, ENOENT, ENOEXEC, ENOMEM, ENOTDIR, EPERM, EPERM, EPERM, ETXTBSY);
-}
-
