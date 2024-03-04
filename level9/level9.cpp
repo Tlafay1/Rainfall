f1265d8cd1c7a53c7195a2fb912ea6a7eff504da
Nard Théo <58373303+PandeoF1@users.noreply.github.com>
2024-03-04 09:40:39 +0000

    oui

--- level9/level9.cpp
+++ level9/level9.cpp
@@ -1,53 +1,39 @@
+#include <unistd.h>
+#include <cstring>
 
-void *operator_new(uint param_1)
-
+class N
 {
-  void *pvVar1;
-  
-  pvVar1 = operator_new(param_1);
-  return pvVar1;
-}
+	public:
+		int nb;
+		int (N::*func)(N &);
+		char annotation[100];
 
-void main(int argc,  char **argv)
+		N(int val) : nb(val)
+		{
+			this->func = &N::operator+;
+		}
+		int operator+(N &right)
+		{
+			return this->nb + right.nb;
+		}
+		int operator-(N &right)
+		{
+			return this->nb - right.nb;
+		}
+		void setAnnotation(char *str)
+		{
+			memcpy(this->annotation, str, strlen(str));
+		}
+};
+
+int main(int ac, char **av)
 {
-  N *obj1;
-  N *obj2;
-  
-  if (argc < 2) {
-    _exit(1);
-  }
-  obj1 = (N *)operator_new(108);
-  N::N(obj1,5);
-  obj2 = (N *)operator_new(108);
-  N::N((N *)obj2,6);
-  N::setAnnotation(obj1,(*argv)[4]);
-  (**(code **)*obj2)(obj2,obj1);
-  return;
-}
+	if (ac < 1)
+		_exit(1);
 
-void N::N(int param_1)
-{
-  *(int *)(this + 104) = param_1;
-  return;
-}
+	N *a = new N(5);
+	N *b = new N(6);
 
-void N::setAnnotation(char *str)
-{
-  size_t len;
-  
-  len = strlen(str);
-  memcpy(this + 4,str,len);
-  return;
-}
-
-int N::operator_(N *param_1)
-
-{
-  return *(int *)(param_1 + 0x68) + *(int *)(this + 0x68);
-}
-
-int N::operator_(N *param_1)
-
-{
-  return *(int *)(this + 0x68) - *(int *)(param_1 + 0x68);
+	a->setAnnotation(av[1]);
+	return (b->*(b->func))(*a);
 }
